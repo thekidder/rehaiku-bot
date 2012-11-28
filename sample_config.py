f9abdac1e365eefa963c0609c48191ec19f65231
Adam Kidder <thekidder@gmail.com>
2012-11-28 09:27:00 -0800

    sample config

---
+++ sample_config.py
@@ -0,0 +1,10 @@
+import irc.bot
+
+# Sample config
+# Rename to config.py and edit as desired
+
+servers = [irc.bot.ServerSpec('irc.freenode.org', 6667)]
+nick    = 'rehaiku'
+name    = 'Rehaiku IRC Bot'
+channel = '#rehaiku'
+ssl     = False

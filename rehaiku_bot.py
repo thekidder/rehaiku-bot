e512418ae60fa2bb73e6c5ca3334fe1bb8928003
Adam Kidder <thekidder@gmail.com>
2012-11-27 21:28:21 -0800

    some cleanup

--- rehaiku_bot.py
+++ rehaiku_bot.py
@@ -9,14 +9,12 @@ class RehaikuBot(irc.bot.SingleServerIRCBot):
     def __init__(self, server_list, nick, name, channel, recon_interval=60, **connect_params):
         super(RehaikuBot, self).__init__(server_list, nick, name, recon_interval, **connect_params)
         self.channel = channel
-        self.connection.add_global_handler("welcome", self._on_connect, -20)
 
 
-    def _on_connect(self, c, e):
+    def on_welcome(self, c, e):
         logger.info("Connected to %s", e.source)
-        self.connection.join(self.channel)
+        c.join(self.channel)
 
 
-    def _on_join(self, c, e):
-        super(RehaikuBot, self)._on_join(c, e)
+    def on_join(self, c, e):
         logger.info("Joined channel %s", e.target)

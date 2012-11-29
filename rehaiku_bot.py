19d7e42a94295cc39deecd722677d56d7c8b0fd9
Adam Kidder <thekidder@gmail.com>
2012-11-28 20:34:56 -0800

    database for collecting text, one working command

--- rehaiku_bot.py
+++ rehaiku_bot.py
@@ -3,6 +3,7 @@ import logging
 import irc.bot
 
 import config
+import textdb
 
 
 logger = logging.getLogger(__name__)
@@ -11,7 +12,8 @@ class RehaikuBot(irc.bot.SingleServerIRCBot):
     def __init__(self, server_list, nick, name, channel, recon_interval=60, **connect_params):
         super(RehaikuBot, self).__init__(server_list, nick, name, recon_interval, **connect_params)
         self.channel = channel
-        self.cmds = ['numlines','haiku']
+        self.cmds = ['stats','haiku']
+        self.db = textdb.TextDb()
 
 
     def on_welcome(self, c, e):
@@ -24,31 +26,37 @@ class RehaikuBot(irc.bot.SingleServerIRCBot):
 
 
     def on_pubmsg(self, c, e):
-        logger.info("Got public msg {} (from {}, to {})".format(e.arguments, e.source.nick, e.target))
+        logger.debug("Got public msg {} (from {}, to {})".format(e.arguments, e.source.nick, e.target))
         self._process_msg(e.target, e)
 
 
     def on_privmsg(self, c, e):
-        logger.info("Got private msg {} (from {}, to {})".format(e.arguments, e.source.nick, e.target))
+        logger.debug("Got private msg {} (from {}, to {})".format(e.arguments, e.source.nick, e.target))
         self._process_msg(e.source.nick, e)
 
 
     def _process_msg(self, respond_target, e):
-        cmd = self._get_cmd(e.arguments)
+        full_text = e.arguments[0]
+        cmd = self._get_cmd(full_text)
         if cmd != None:
             self._process_cmd(respond_target, cmd, e)
-        elif e.arguments[:1] != config.cmd_prefix:
+        elif full_text[:1] != config.cmd_prefix:
             self._process_text(e)
-        self.connection.privmsg(respond_target, "pong!")
 
 
     def _process_text(self, e):
-        logger.debug("_process_text: " + e.arguments)
+        txt = e.arguments[0]
+        logger.debug("_process_text: " + txt)
+        self.db.add_line(e.source.nick, e.target, txt)
 
 
     def _get_cmd(self, cmd):
         if cmd[:1] == config.cmd_prefix:
+            cmd_boundary = cmd.find(' ')
             cmd = cmd[1:]
+            if cmd_boundary != -1:
+                cmd = cmd[:cmd_boundary]
+
             if cmd in self.cmds:
                 return cmd
 
@@ -56,13 +64,18 @@ class RehaikuBot(irc.bot.SingleServerIRCBot):
 
 
     def _process_cmd(self, respond_target, cmd, e):
-        logger.debug("_process_cmd: {} ({})".format(cmd, e.arguments))
+        logger.debug("_process_cmd: {} ({})".format(cmd, e.arguments[0]))
         getattr(self, '_do_' + cmd)(respond_target, cmd, e)
 
 
-    def _do_numlines(self, respond_target, cmd, e):
-        logger.debug("_do_numlines")
+    def _do_stats(self, respond_target, cmd, e):
+        logger.debug("_do_stats")
+        sender = e.source.nick
+        count = self.db.get_line_count_by_nick(sender)
+        self.connection.privmsg(respond_target, '{}: I have collected {} lines of dialog from you.'.format(
+                sender, count))
 
 
     def _do_haiku(self, respond_target, cmd, e):
         logger.debug("_do_haiku")
+        self.connection.privmsg(respond_target, "I'm sorry, Dave. I'm afraid I can't do that.")
---
+++ textdb.py
@@ -0,0 +1,54 @@
+import datetime
+import logging
+import sqlite3
+
+
+logger = logging.getLogger(__name__)
+
+class TextDb:
+    def __init__(self, db_name='text.db'):
+        self.db_name = db_name
+        self.table_name = 'text'
+        self._connect()
+
+
+    def _connect(self):
+        self.connection = sqlite3.connect(self.db_name)
+        if not self._exists():
+            self._create()
+
+
+    def _create(self):
+        sql = '''create table {} (date text, nick text, target text, full_text text)'''.format(self.table_name)
+        c = self.connection.cursor()
+        c.execute(sql)
+        self.connection.commit()
+
+
+    def _exists(self):
+        sql = '''select count(type) from sqlite_master where type='table' and name=?'''
+        c = self.connection.cursor()
+        c.execute(sql, (self.table_name,))
+        return c.fetchone()[0] == 1
+
+
+    def get_lines_by_nick(self, nick):
+        sql = '''select full_text from {} where nick=? order by date asc'''.format(self.table_name)
+        c = self.connection.cursor()
+        c.execute(sql, (nick,))
+        return c
+
+
+    def get_line_count_by_nick(self, nick):
+        sql = '''select count(full_text) from {} where nick=?'''.format(self.table_name)
+        c = self.connection.cursor()
+        c.execute(sql, (nick,))
+        return c.fetchone()[0]
+
+
+    def add_line(self, nick, target, full_text):
+        sql = '''insert into {} values (?,?,?,?)'''.format(self.table_name)
+        now = datetime.datetime.utcnow().isoformat()
+        c = self.connection.cursor()
+        c.execute(sql, (now,nick,target,full_text,))
+        self.connection.commit()

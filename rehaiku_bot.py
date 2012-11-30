55027c57653686887d52aa021d9ef42246885457
Adam Kidder <thekidder@gmail.com>
2012-11-30 14:14:55 -0800

    ability to @stats user

--- rehaiku_bot.py
+++ rehaiku_bot.py
@@ -37,9 +37,9 @@ class RehaikuBot(irc.bot.SingleServerIRCBot):
 
     def _process_msg(self, respond_target, e):
         full_text = e.arguments[0]
-        cmd = self._get_cmd(full_text)
+        cmd,arguments = self._get_cmd(full_text)
         if cmd != None:
-            self._process_cmd(respond_target, cmd, e)
+            self._process_cmd(respond_target, cmd, arguments, e)
         elif full_text[:1] != config.cmd_prefix:
             self._process_text(e)
 
@@ -54,34 +54,44 @@ class RehaikuBot(irc.bot.SingleServerIRCBot):
         if cmd[:1] == config.cmd_prefix:
             cmd_boundary = cmd.find(' ')
             cmd = cmd[1:]
+            cmd_name = cmd
+            arguments = ""
             if cmd_boundary != -1:
-                cmd = cmd[:cmd_boundary]
+                cmd_name = cmd[:cmd_boundary].strip()
+                arguments = cmd[cmd_boundary:].strip()
 
-            if cmd in self.cmds:
-                return cmd
+            if cmd_name in self.cmds:
+                logger.info("got command name " + cmd_name + " " + arguments)
+                return cmd_name,arguments
 
-        return None
-
-
-    def _process_cmd(self, respond_target, cmd, e):
-        logger.debug("_process_cmd: {} ({})".format(cmd, e.arguments[0]))
-        getattr(self, '_do_' + cmd)(respond_target, cmd, e)
+        return None,None
 
 
-    def _do_stats(self, respond_target, cmd, e):
+    def _process_cmd(self, respond_target, cmd, arguments, e):
+        logger.debug("_process_cmd: {} ({})".format(cmd, arguments))
+        getattr(self, '_do_' + cmd)(respond_target, cmd, arguments, e)
+
+
+    def _do_stats(self, respond_target, cmd, arguments, e):
         logger.debug("_do_stats")
         sender = e.source.nick
-        count = self.db.get_line_count_by_nick(sender)
-        self.connection.privmsg(respond_target, '{}: I have collected {} lines of dialog from you.'.format(
-                sender, count))
+
+        if len(arguments.strip()) == 0:
+            user = sender
+        else:
+            user = arguments
+
+        count = self.db.get_line_count_by_nick(user)
+        self.connection.privmsg(respond_target, 'I have collected {} lines of dialog from {}.'.format(
+                count, user))
 
 
-    def _do_haiku(self, respond_target, cmd, e):
+    def _do_haiku(self, respond_target, cmd, arguments, e):
         logger.debug("_do_haiku")
         self.connection.privmsg(respond_target, "I'm sorry, Dave. I'm afraid I can't do that.")
 
 
-    def _do_replay(self, respond_target, cmd, e):
+    def _do_replay(self, respond_target, cmd, arguments, e):
         logger.debug("_do_replay")
         sender = e.source.nick
         line = self.db.get_random_line(sender,e.target)
